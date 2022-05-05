5af0319e17aa40754d0376ac0e6e09ec04443b49
Hoerrin <patrykd0604@gmail.com>
2022-05-05 23:19:33 +0200

    Changes to layout sizing; Hiding popUp

--- src/components/AddTaskPU.js
+++ src/components/AddTaskPU.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addTask } from '../features/Tasks/tasksListSlice'
 import './AddTaskPU.css'
 
-function AddTaskPU() {
+function AddTaskPU(props) {
   const tags = useSelector((state) => state.tagsList)
   const dispatch = useDispatch()
 
@@ -27,6 +27,7 @@ function AddTaskPU() {
 
     if (taskTitle){
       dispatch(addTask({ taskTitle, taskDescription, taskTag }))
+      props.switchPopUp()
       return
     }
     
@@ -41,7 +42,7 @@ function AddTaskPU() {
   }
 
   return (
-    <div className='addTaskPU__container'>
+    <div className='addTaskPU addTaskPU--hidden'>
       <label className='addTaskPU__label'>Task name <input type="text" required="required" className='addTaskPU__input' maxLength={200} value={taskNameInput} onChange={(e) => setTaskNameInput(e.target.value)} /></label>
       <label className='addTaskPU__label'>Description <textarea type='textarea' placeholder='optional' className='addTaskPU__input addTaskPU__input--textarea' maxLength={500} value={taskDescrInput} onChange={(e) => setTaskDescrInput(e.target.value)} /></label>
       <label className='addTaskPU__label'>Tag <select name="taskTag" className='addTaskPU__input' value={taskTagInput} onChange={(e) => seTtaskTagInput(e.target.value)}>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Main.css';
 import { TasksList } from './TasksList'
 import { useDispatch, useSelector } from "react-redux";
@@ -9,17 +9,30 @@ function Main() {
     const dispatch = useDispatch()
     const filter = useSelector((state) => state.filterTasks)
 
+    const [showPopUp, setShowPopUp] = useState(false);
+
+    const switchPopUp = () => {
+        let popUp = document.getElementsByClassName("addTaskPU")[0]
+
+        setShowPopUp(!showPopUp)
+
+        if (showPopUp) {
+            popUp.classList.add("addTaskPU--hidden")
+        } else {
+            popUp.classList.remove("addTaskPU--hidden")
+        }
+    }
 
     return (
         <div className="main">
             <div className='main_blur'></div>
-            <AddTaskPU />
+            <AddTaskPU switchPopUp={switchPopUp} />
             <div className="main__header">
                 <div className='main__headerContainer'>
                     <h1>My Day</h1>
-                    <button className='main__addTagButton'>+</button>
+                    <button onClick={switchPopUp} className='main__addTagButton'>+</button>
                 </div>
-                <label><input type="checkbox" checked={filter.showCompleted} onChange={() => dispatch(changeShowCompleted())}></input>Show completed</label>
+                <label><input type="checkbox" checked={filter.showCompleted} onChange={() => dispatch(changeShowCompleted())}></input> Show completed</label>
             </div>
             <TasksList />
         </div>
